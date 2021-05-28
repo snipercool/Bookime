6c31c34892ef276dd624c5bb10bd9e5de1df5c0e
larspauwels <lars.pauwels@telenet.be>
2021-05-29 00:53:14 +0200

    firebase npm fix

--- controllers/Firebase.js
+++ controllers/Firebase.js
@@ -1,6 +1,7 @@
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "/firebase/app";
+import { getFirestore } from "/firebase/firestore";
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyBlzzBYLsJ_hrOvIB_X4SCpnl_31i-RP6A",
   authDomain: "bookime-38af1.firebaseapp.com",
   projectId: "bookime-38af1",
@@ -8,10 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "649459719619",
   appId: "1:649459719619:web:829f6c3ff843bad7e8f176",
   measurementId: "G-BESH9004QK"
-};
+});
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 const db = getFirestore();
