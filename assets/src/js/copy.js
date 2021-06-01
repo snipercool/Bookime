f8ac96aecc0f491768fc3626e54459b56ae7125b
snipercool <44378067+snipercool@users.noreply.github.com>
2021-06-01 13:51:51 +0200

    Login/Registe front-end Done

--- assets/src/js/copy.js
+++ assets/src/js/copy.js
@@ -3,8 +3,11 @@ const link = document.getElementById('animeLink');
 document.addEventListener('DOMContentLoaded', function () {
     copybtn.addEventListener('click', function(){
       if (link != "" || link != "undefined" || link != null) {
-          link.querySelector();
+          link.type = "text";
+          link.select();
           document.execCommand("copy");
+          link.type = "hidden";
+          console.log(link.value);
       }
     }, false);
   })
