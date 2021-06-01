6850e2359c8455e53cd2a35e8b15c0c4ae56f255
snipercool <44378067+snipercool@users.noreply.github.com>
2021-06-01 10:57:54 +0200

    Login/Registe front-end Done

--- assets/src/js/tabs.js
+++ assets/src/js/tabs.js
@@ -1,31 +1,34 @@
 const Login = document.getElementById('LoginBtn');
 const Register = document.getElementById('RegisterBtn');
 
-Login.addEventListener('click', openTab("Login", Login));
-
-Register.addEventListener('click', openTab("Register", Register));
-
-function openTab(Name, btn) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
+document.addEventListener('DOMContentLoaded', function () {
+  Register.addEventListener('click', function(){
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+      tabcontent[i].classList.remove("show");
     }
-    console.log("hidden");
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    if (Login.classList.contains('active')) {
+      Login.classList.remove("active")
     }
-    console.log("none-active");
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(Name).style.display = "block";
-    btn.className += " active";
-    console.log("activated");
-  }
+    document.getElementById("Register").classList.add("show");
+    Register.classList.add("active");
+  }, false);
+
+  Login.addEventListener('click', function(){
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].classList.remove("show");
+    }
+    if (Register.classList.contains('active')) {
+      Register.classList.remove("active")
+    }
+    document.getElementById("Login").classList.add("show");
+    Login.classList.add("active");
+  }, false);
+  Login.click();
+})
+
+
+
+
 
