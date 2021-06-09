d9d21e1a257e1561d84ab8ec587d6e12b560f2d6
snipercool <44378067+snipercool@users.noreply.github.com>
2021-06-09 23:35:30 +0200

    socials fix + giving titles to footer links cause who doesnt want to be a lord or lady

--- assets/src/js/faq.js
+++ assets/src/js/faq.js
@@ -4,13 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
         item.addEventListener('click', (e) => {
             const et = e.target.closest('.bookime__content__account__faq__loop--item');
 
-            if (et.classList.contains("open")) {
-               
-                et.classList.remove("open");
-            }else{
-                et.classList.add("open");
+            if (et.classList.contains('open')) {
+                item.classList.remove('open');
+            } else {
+                for (let item of faqItem) {
+                    item.classList.remove('open');                    
+                }
+                et.classList.add('open')
             }
-            
+
         })
     }
-  })
+})
